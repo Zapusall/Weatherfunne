ad1a45aac6f6ecf075cf9f0f488aa3853e333365
Zapusall <104385875+Zapusall@users.noreply.github.com>
2022-04-25 13:14:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ if 'ls' not in st.session_state:
 	
 @st.cache
 def load_data(city):
-    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=f4f19cae7d4100d1cceb1599f25c750e&units=imperial").json()
+    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=APIKEYHERE&units=imperial").json()
 
 data = [x for y in list(json.load(open('cities.json')).values()) for x in y]
 
 selected_city = st.selectbox("Select a city", data)
 loaded = load_data(selected_city)['main']
-st.write(loaded)
 st.session_state.ls[selected_city] = loaded
-st.table(st.session_state.ls)
