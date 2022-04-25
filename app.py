e5ae74cbf0d734dd0883847bce85821e4e5702b4
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-25 12:47:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ data = [x for y in list(json.load(open('cities.json')).values()) for x in y]
 selected_city = st.selectbox("Select a city", data)
 loaded = load_data(selected_city)['main']
 st.write(loaded)
-st.session_state.ls[selected_city] = [loaded]
+st.session_state.ls[selected_city] = {loaded}
 st.write(st.session_state.ls)
