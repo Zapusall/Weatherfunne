c5c37f13e55cd3eb04cd2cba669ee00edfde5f49
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-25 12:33:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from datetime import date
 from plotly import graph_objs as go
@@ -5,12 +6,16 @@ import requests
 import json
 import itertools
 
+ls = []
+
 @st.cache
 def load_data(city):
     return requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=f4f19cae7d4100d1cceb1599f25c750e&units=imperial").json()
 
 data = [x for y in list(json.load(open('cities.json')).values()) for x in y]
 
-selected_stock = st.selectbox("Select a city", data)
+selected_city = st.selectbox("Select a city", data)
 
-st.write(load_data(selected_stock)['main'])
+st.write(load_data(selected_city)['main'])
+ls.append(selected_city)
+st.write(ls)
